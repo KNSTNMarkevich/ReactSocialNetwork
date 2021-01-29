f1d78ba425c19e28d561bcc15707a40364917790
KNSTNMarkevich <konstantinmarkevich@gmail.com>
2021-01-29 20:01:44 +0300

    feat: add Header, Navbar, Profile components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://lh3.googleusercontent.com/proxy/d5c_CghranGZZ2DlJBZijXUTjVw_FOIJuMSoQzY4IrkWIhSMXw68MYehdiCWkNz-266B1SfB72vm6xYXeLRioljR4OtUTqcDG2SqBPhBTFxfyo8XoJaeU2nObFDGfw' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdn.theculturetrip.com/wp-content/uploads/2013/04/rome-e1487329583205.jpg'></img>
+            </div>
+            <div >
+                avarar+descr
+        </div>
+            <div>
+                My Posts
+            <div>
+                    New post
+            </div>
+                <div>
+                    Post 1
+            </div>
+                <div>
+                    Post 2
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
