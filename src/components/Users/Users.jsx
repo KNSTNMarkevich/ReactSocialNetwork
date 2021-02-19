aacbf76ad2fd2ef7c42434c6b4326d8a1a907151
KNSTNMarkevich <konstantinmarkevich@gmail.com>
2021-02-20 01:30:09 +0300

    feat: added lifecicle method componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,24 +7,41 @@ class Users extends React.Component {
 
     componentDidMount() {
         axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
 
-    // [
-        //     {id: 1, photoUrl: 'https://sun2.velcom-by-minsk.userapi.com/s/v1/ig1/3nBH_b9o5ZymRS3TlxFHG7XQMa7dRq0FLA16DLSnFdgdyKcbPrtjMGUwGuEKDqQ-xnvucG66.jpg?size=50x0&quality=96&crop=0,308,1080,1080&ava=1', followed: false, fullName: 'Konstantin', status: 'cold', location: {city: 'Minsk', country: 'Belarus'}},
-        //     {id: 2, photoUrl: 'https://sun2.velcom-by-minsk.userapi.com/s/v1/ig1/3nBH_b9o5ZymRS3TlxFHG7XQMa7dRq0FLA16DLSnFdgdyKcbPrtjMGUwGuEKDqQ-xnvucG66.jpg?size=50x0&quality=96&crop=0,308,1080,1080&ava=1', followed: true, fullName: 'Konstantin', status: 'cold', location: {city: 'Moscow', country: 'Russia'}},
-        //     {id: 3, photoUrl: 'https://sun2.velcom-by-minsk.userapi.com/s/v1/ig1/3nBH_b9o5ZymRS3TlxFHG7XQMa7dRq0FLA16DLSnFdgdyKcbPrtjMGUwGuEKDqQ-xnvucG66.jpg?size=50x0&quality=96&crop=0,308,1080,1080&ava=1', followed: false, fullName: 'Konstantin', status: 'cold', location: {city: 'Ukraine', country: 'Kiev'}},
-        //     {id: 4, photoUrl: 'https://sun2.velcom-by-minsk.userapi.com/s/v1/ig1/3nBH_b9o5ZymRS3TlxFHG7XQMa7dRq0FLA16DLSnFdgdyKcbPrtjMGUwGuEKDqQ-xnvucG66.jpg?size=50x0&quality=96&crop=0,308,1080,1080&ava=1', followed: true, fullName: 'Konstantin', status: 'cold', location: {city: 'Minsk', country: 'Belarus'}},
-        // ]
-        render()
-        {
-            return (
-                <div>
-                    {
-                        this.props.users.map(u => <div key={u.id}>
+    render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+        return (
+            <div>
+                {
+                    pages.map(p => {
+                        return <span
+                            className={this.props.currentPage === p && style.selectedPage}
+                            onClick={(e) => {
+                                this.onPageChanged(p);
+                            }}>{p}</span>
+                    })
+                }
+                {
+                    this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
@@ -39,7 +56,7 @@ class Users extends React.Component {
                                 }}>Unfollow</button>}
                         </div>
                     </span>
-                            <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -49,11 +66,11 @@ class Users extends React.Component {
                             <div>{"u.location.country"}</div>
                         </span>
                     </span>
-                        </div>)
-                    }
-                </div>
-            )
-        }
+                    </div>)
+                }
+            </div>
+        )
     }
+}
 
 export default Users;
